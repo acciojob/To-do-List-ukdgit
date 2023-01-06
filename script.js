508dd01ae745beee8fc2c7b8d421b42a48ab587f
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-06 19:36:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-//your code here
+const input = document.getElementById('newTodoInput')
+const button = document.getElementById('addTodoBtn')
+const ol = document.getElementById('todoList')
 
+const onClick = () => {
+    const todo = input.value
+    if (!todo) return;
+
+    const li = document.createElement('li')
+    li.innerText = todo
+    ol.appendChild(li)
+    input.value = ''
+}
+
+button.addEventListener('click', onClick)
